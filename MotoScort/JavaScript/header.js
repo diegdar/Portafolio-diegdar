3853a76082884989355307907723698e2f2c0488
Diego Chacon <diegdarch@hotmail.com>
2023-08-31 15:57:22 +0200

    color ultimo link cliqueado menu navegacion

--- MotoScort/JavaScript/header.js
+++ MotoScort/JavaScript/header.js
@@ -12,17 +12,31 @@ navToggle.addEventListener("click", () =>{
     }
 })
 
-//🆕 Mantiene el color del ultimo link clequeado 
-const menuLinks = document.querySelectorAll('.nav_menu a');
+//🆕 Mantiene el color del ultimo link cliqueado en la primera linea del menu de navegacion
+// Obtenemos el enlace de la página actual
+var currentPage = parent.window.location.href;
 
-menuLinks.forEach(link => {
-  link.addEventListener('click', function() {
-    // Eliminar la clase 'clicked' de todos los enlaces
-    menuLinks.forEach(link => {
-      link.classList.remove('itemActive');
+// Obtenemos todos los elementos del menú
+var menuItems = document.getElementsByClassName("nav_menu_item");
+
+// Recorremos los elementos del menú y verificamos si el enlace coincide con la página actual
+let long = menuItems.length;
+for (var i = 0; i < long; i++) {
+  var menuItem = menuItems[i].getElementsByTagName("a")[0];
+  if (menuItem.href.includes(currentPage)) {
+    menuItem.classList.add('itemActive'); /* Cambia el color al elemento actual */
+  }
+}
+//🆕 Mantiene el color del ultimo link cliqueado en la segundalinea del menu(subMenu) de navegacion
+const links2ndLine = document.querySelectorAll('.nav_2ndLine a');
+
+links2ndLine.forEach(link => {
+    link.addEventListener('click', function() {
+      // Eliminar la clase 'itemActive' de todos los enlaces
+      links2ndLine.forEach(link => {
+        link.classList.remove('itemActive');
+      });
+      // Agregar la clase 'itemActive' solo al enlace clicado
+      this.classList.add('itemActive');
     });
-
-    // Agregar la clase 'clicked' solo al enlace clicado
-    this.classList.add('itemActive');
   });
-});
